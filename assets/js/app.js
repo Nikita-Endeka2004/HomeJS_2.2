6eb843af9508f57e287a7d639baa22f4a144547f
Nikita Endea <90311455+Nikita-Endeka2004@users.noreply.github.com>
2022-09-18 16:46:49 +0300

    jsUpdate

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,7 @@ let grn = {
 
 let ForUnit = {
 
-    1: "",
+    1: "Одна",
     2: "Дві",
     3: "Три",
     4: "Чотири",
@@ -58,61 +58,96 @@ let ForNoun = {
 
 let ForTen = {
 
-
+    10: "Десять",
+    11: "Одинадцять",
+    12: "Дванадцять",
+    13: "Тринадяцять",
+    14: "Чотирнадцять",
+    15: "П'ятнадцять",
+    16: "Шістнадцять",
+    17: "Сімнадцять",
+    18: "Вісімнадцять",
+    19: "Дев'ятнадцять"
 
 }
 
-let number = '190';
-
+    
 let varible;
 
-let str = number.split('');
+function WordTranslat(){
 
-if((number % 100) > 9){
-
-    varible = ForHundreds[str[0]];
-
-    number = number % 100;
-
-    console.log(number);
-
-    if((number % 10) == 0){
-
-        varible = varible + " " + ForNoun[str[1]];
-
-        number = number % 10;
-
-        if(number > 0){
-
-            varible = varible + " " + ForUnit[str[2]];
-
-            varible = varible + " " + grn[str[2]];
-
-            console.log(varible);
-
+    let number = numberInput.value;
+    
+    let str = number.split('');
+    
+    str = str.reverse();
+    
+    if(str[2]){ //if have hundreds
+    
+        varible = ForHundreds[str[2]];
+    
+        if(str[1] == 0){  //if second numder = 0
+    
+            varible = varible + " " + ForUnit[str[0]] + " " + grn[str[0]];
+    
         }
-
+    
+        if(str[1] == 1){ //if second number = 0
+    
+            let value = str[1] + str[0];
+    
+            varible = varible + " " + ForTen[value] + " гривень" ;
+    
+        }
+    
+        if((str[1] != 1) && (str[1] != 0)){ //else
+    
+            if(str[0] == 0){
+    
+                varible = varible + " " + ForNoun[str[1]] + " гривень";
+    
+            }else{
+    
+                varible = varible + " " + ForNoun[str[1]] + " " + ForUnit[str[0]] + " " + grn[str[0]];
+    
+            }
+    
+        }
+    
     }
+    
+    if(str[2] == undefined){
+    
+        if(str[1] == 1){ 
+    
+            let value = str[1] + str[0];
+    
+            varible = ForTen[value] + " гривень" ;
+    
+        }
+    
+        else{ 
+    
+            if(str[0] == 0){
+    
+                varible = ForNoun[str[1]] + " гривень";
+    
+            }else{
+    
+                varible = ForNoun[str[1]] + " " + ForUnit[str[0]] + " " + grn[str[0]];
+    
+            }
+    
+        }
+    
+    }
+    
+    if(str[1] == undefined){
+    
+        varible = ForUnit[str[0]] + " " + grn[str[0]];
+    
+    }
+    
+    indexInput.innerHTML = `${varible}`;
 
 }
-
-// if((number % 100)){
-
-
-// }
-
-if(number == 100){
-
-    varible = ForHundreds[str[0]];
-
-    varible = varible + " " + grn[str[2]];
-
-    console.log(varible);
-
-}
-
-// number = number % 100;
-
-// number = number % 10;
-
-// console.log(number);
